fe82e6b4232de158a936f18266ae5bb0b13156ec
KSFirstUser <rusakk@mail.ru>
2018-11-27 08:51:25 +0300

    last changes

--- HW-3/buildMenu.php
+++ HW-3/buildMenu.php
@@ -1,4 +1,8 @@
-<!DOCTYPE html>
+
+
+
+<?php
+$textOfSite = '<!DOCTYPE html>
 
 <html>
     <head>
@@ -24,126 +28,122 @@
         }
 
 
-
     </style>
     <body>
+     {{MENU}}
+    </body>
+</html>';
 
 
-        <?php
-        /*
-         * To change this license header, choose License Headers in Project Properties.
-         * To change this template file, choose Tools | Templates
-         * and open the template in the editor.
-         */
 
-        $elemOfMenu = [
-            "class" => "nav flex-column",
-            "li" => [
+$elemOfMenu = [
+    "class" => "nav flex-column",
+    "li" => [
+        [
+            "class" => "nav-item",
+            "a" => ["class" => "nav-link active",
+                "href" => "#",
+                "text" => "Active"]
+        ]
+        ,
+        [
+            "class" => "nav-item",
+            "a" => ["class" => "nav-link is-submenu",
+                "href" => "#",
+                "text" => "link1 with submenu"],
+            "submenu" =>
+            ["class" => "nav flex-column submenu",
+                "li" =>
                 [
-                 "class" => "nav-item",
-                 "a" => ["class" => "nav-link active",
-                        "href" => "#",
-                        "text" => "Active"]
-                ]
-                , 
-                [
-                 "class" => "nav-item",
-                 "a" => ["class" => "nav-link is-submenu",
-                        "href" => "#",
-                        "text" => "link1 with submenu"],
-                 "submenu" =>
-                        ["class" => "nav flex-column submenu",
-                            "li" =>
-                                    [
-                                        [
-                                            "class" => "nav-item",
-                                            "a" => ["class" => "nav-link",
-                                            "href" => "#",
-                                            "text" => "Submenu Link1"]
-                                        ],
-                                        [
-                                            "class" => "nav-item",
-                                            "a" => ["class" => "nav-link",
-                                            "href" => "#",
-                                            "text" => "Submenu link2"]
-                                        ]
-                                    ]
-                        ]
-                ],
-                [
-                 "class" => "nav-item",
-                 "a" => ["class" => "nav-link",
-                        "href" => "#",
-                        "text" => "link2"]
+                    [
+                        "class" => "nav-item",
+                        "a" => ["class" => "nav-link",
+                            "href" => "#",
+                            "text" => "Submenu Link1"]
+                    ],
+                    [
+                        "class" => "nav-item",
+                        "a" => ["class" => "nav-link",
+                            "href" => "#",
+                            "text" => "Submenu link2"]
+                    ]
                 ]
             ]
-        ];
+        ],
+        [
+            "class" => "nav-item",
+            "a" => ["class" => "nav-link",
+                "href" => "#",
+                "text" => "link2"]
+        ]
+    ]
+];
 
-        $ulTemplate = "<ul {{CLASS}}>{{LI}}</ul>";
-        $liTemplate = "<li {{LI-CLASS}}><a {{A-CLASS}} {{A-HREF}}>{{A-TEXT}}</a> {{SUBMENU}} </li>";
+$ulTemplate = "<ul {{CLASS}}>{{LI}}</ul>";
+$liTemplate = "<li {{LI-CLASS}}><a {{A-CLASS}} {{A-HREF}}>{{A-TEXT}}</a> {{SUBMENU}} </li>";
 
-        function buildMenuLi($valueLi, $liTemplate, $ulTemplate) {
+function buildMenuLi($valueLi, $liTemplate, $ulTemplate) {
 
-            $menuOfLi = '';
-            foreach ($valueLi as $value) {
-                $htmlLiMenu = $liTemplate;
-                foreach ($value as $keyOfValue => $valueOfValue) {
+    $menuOfLi = '';
+    foreach ($valueLi as $value) {
+        $htmlLiMenu = $liTemplate;
+        foreach ($value as $keyOfValue => $valueOfValue) {
 
-                    if ($keyOfValue === "class") {
-                        $subject = $keyOfValue . "=" . '"' . $valueOfValue . '"';
-                        $htmlLiMenu = str_replace("{{LI-CLASS}}", $subject, $htmlLiMenu);
-                    };
-
-                    if ($keyOfValue === "a") {
-                        foreach ($valueOfValue as $kOfV => $vOfV) {
-
-                            if ($kOfV === "class") {
-                                $subject = $kOfV . "=" . '"' . $vOfV . '" ';
-                                $htmlLiMenu = str_replace("{{A-CLASS}}", $subject, $htmlLiMenu);
-                            };
-                            if ($kOfV === "href") {
-                                $subject = $kOfV . "=" . '"' . $vOfV . '" ';
-                                $htmlLiMenu = str_replace("{{A-HREF}}", $subject, $htmlLiMenu);
-                            };
-                            if ($kOfV === "text") {
-                                $subject = $vOfV;
-                                $htmlLiMenu = str_replace("{{A-TEXT}}", $subject, $htmlLiMenu);
-                            };
-                        }
-                    }
-                    if ($keyOfValue === "submenu") {
-                        $subMenu = buildMenu($valueOfValue, $ulTemplate, $liTemplate);
-                        $htmlLiMenu = str_replace("{{SUBMENU}}", $subMenu, $htmlLiMenu);
-                    }
-                }
-
-                $htmlLiMenu = str_replace("{{SUBMENU}}", '', $htmlLiMenu);
-                $menuOfLi .= $htmlLiMenu;
+            if ($keyOfValue === "class") {
+                $subject = $keyOfValue . "=" . '"' . $valueOfValue . '"';
+                $htmlLiMenu = str_replace("{{LI-CLASS}}", $subject, $htmlLiMenu);
             }
-            return $menuOfLi;
-        }
 
-        function buildMenu($elemOfMenu, $ulTemplate, $liTemplate) {
-            $htmlMenu = $ulTemplate;
-            $liOFMenu = '';
-            foreach ($elemOfMenu as $key => $value) {
+            if ($keyOfValue === "a") {
+                foreach ($valueOfValue as $kOfV => $vOfV) {
 
-                if ($key === "class") {
-                    $subject = $key . '="' . $value . '"';
-                    $htmlMenu = str_replace("{{CLASS}}", $subject, $htmlMenu);
-                }
-                if ($key === "li") {
-                    $liOFMenu .= buildMenuLi($value, $liTemplate, $ulTemplate);
+                    if ($kOfV === "class") {
+                        $subject = $kOfV . "=" . '"' . $vOfV . '" ';
+                        $htmlLiMenu = str_replace("{{A-CLASS}}", $subject, $htmlLiMenu);
+                    }
+                    if ($kOfV === "href") {
+                        $subject = $kOfV . "=" . '"' . $vOfV . '" ';
+                        $htmlLiMenu = str_replace("{{A-HREF}}", $subject, $htmlLiMenu);
+                    }
+                    if ($kOfV === "text") {
+                        $subject = $vOfV;
+                        $htmlLiMenu = str_replace("{{A-TEXT}}", $subject, $htmlLiMenu);
+                    }
                 }
             }
-            $htmlMenu = str_replace("{{LI}}", $liOFMenu, $htmlMenu);
-            ;
-            return $htmlMenu;
+            if ($keyOfValue === "submenu") {
+                $subMenu = buildMenu($valueOfValue, $ulTemplate, $liTemplate);
+                $htmlLiMenu = str_replace("{{SUBMENU}}", $subMenu, $htmlLiMenu);
+            }
         }
 
-        $menu = buildMenu($elemOfMenu, $ulTemplate, $liTemplate);
-        echo $menu;
-        ?>
-    </body>
-</html>
+        $htmlLiMenu = str_replace("{{SUBMENU}}", '', $htmlLiMenu);
+        $menuOfLi .= $htmlLiMenu;
+    }
+    return $menuOfLi;
+}
+
+function buildMenu($elemOfMenu, $ulTemplate, $liTemplate) {
+    $htmlMenu = $ulTemplate;
+    $liOFMenu = '';
+    foreach ($elemOfMenu as $key => $value) {
+
+        if ($key === "class") {
+            $subject = $key . '="' . $value . '"';
+            $htmlMenu = str_replace("{{CLASS}}", $subject, $htmlMenu);
+        }
+        if ($key === "li") {
+            $liOFMenu .= buildMenuLi($value, $liTemplate, $ulTemplate);
+        }
+    }
+    $htmlMenu = str_replace("{{LI}}", $liOFMenu, $htmlMenu);
+    
+    return $htmlMenu;
+}
+
+$menu = buildMenu($elemOfMenu, $ulTemplate, $liTemplate);
+$textOfSite = str_replace("{{MENU}}", $menu, $textOfSite);
+echo $textOfSite;
+?>
+
 
