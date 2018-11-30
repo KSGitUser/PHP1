99ec16f7edceaf43d397e0d6646c612275cc034d
KSFirstUser <rusakk@mail.ru>
2018-11-30 14:58:16 +0300

    Gallery

---
+++ config/main.php
@@ -0,0 +1,19 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+define('ROOT_DIR', $_SERVER['DOCUMENT_ROOT'] . "/");
+define('ENGINE_DIR', ROOT_DIR . "../engine/" );
+define('UPLOADS_DIR', ROOT_DIR . "../uploads/");
+define('TEMPLATES_DIR', ROOT_DIR . "../templates/");
+
+
+
+require_once(ENGINE_DIR . '/functions.php');
+
+
